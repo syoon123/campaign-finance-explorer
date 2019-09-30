33c83b5a360ce9fda47a635cc8dffc33810720e8
Sarah <sarah@zagaran.com>
2019-09-30 17:59:57 -0400

    set up preliminary flask app

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "Welcome!"
+
+@app.route('/campaign_finance_data/<state>', methods=['GET'])
+def campaign_finance_data(state):
+    state = state
+    return render_template('campaign_finance_data.html', state=state)
+
+
+if __name__ == '__main__':
+    app.run()
